302de1f7f0cbc17349ba0d325765a7defb3a1ccc
Denis Tsoi <denistsoi@gmail.com>
2017-11-03 13:56:30 +0800

    Init Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Shrug-emoji
+
+get an emoji and surround it with ascii shrugs.
+inspired by [kxxoling's kaomoji](https://github.com/kxxoling/kaomoji)
+### note
+this has absolutely zero purpose.
+
+### todo
+
+- [x] return closest emoji based on query  
+- [x] copy first shrug-emoji to clipboard  
+- [x] return other words based on query  
+
+### author
+Denis Tsoi <denistsoi@gmail.com>
+
+### license
+MIT
---
+++ app.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const clip = require('cliparoo');
+const emojis = require("emojilib");
+const fuzzy = require('fuzzy');
+
+const inquirer = require('inquirer');
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
+
+// use fuzzy-search to filter emojis
+function searchEmoji(answers, input) {
+	input = input || '';
+	return new Promise(resolve => {
+		setTimeout(() => {
+			let result = fuzzy.filter(input, require('emojilib').ordered);
+			resolve(result.map(el => el.original ));
+		}, 100)
+	})
+}
+
+inquirer.prompt([{ 
+	type: 'autocomplete',
+	name: 'emoji',
+	message: 'type an emoji',
+	source: searchEmoji
+}]).then(function(answers) {
+	let choice = answers.emoji;
+	let emoji = emojis.lib[choice.toString()].char;
+
+	// return shrugged-emoji
+	return `¯\\_\(${emoji}\)_/¯`;
+}).then(shrug => {
+	// copy to clipboard
+	
+	clip(shrug);
+	console.log(`${shrug} has been copied to your clipboard`);
+})
